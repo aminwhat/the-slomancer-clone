ac8fa7a13716cd6a59e2927d99ee523d3523ba0a
aminwhat <m.aminwhat@gmail.com>
2025-08-21 18:34:31 +0330

    auto rebuild plus flip the cursor

--- Scripts/Root.cs
+++ Scripts/Root.cs
@@ -2,17 +2,23 @@ using Godot;
 
 public partial class Root : Node2D
 {
-    private readonly Resource mouse_cursor = ResourceLoader.Load("./Assets/Cursor/Swords_1/Iicon_32_01.png");
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Input.SetCustomMouseCursor(mouse_cursor);
-        GD.Print("Hello from C#");
-    }
+        // Load as Texture2D, not generic Resource
+        var tex = (Texture2D)GD.Load("res://Assets/Cursor/Swords_1/Iicon_32_01.png");
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
+        // Get the underlying image
+        var img = tex.GetImage();
+
+        // Flip it (choose FlipX or FlipY)
+        img.FlipX();
+
+        // Create a new Texture2D from the flipped image
+        var flippedTex = ImageTexture.CreateFromImage(img);
+
+        // Set as custom mouse cursor
+        Input.SetCustomMouseCursor(flippedTex);
+
+        GD.Print("Custom flipped cursor set!");
     }
 }
