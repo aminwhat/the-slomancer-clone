e45c3e774cc8ce529a52759eef364f1922fe6bd3
aminwhat <m.aminwhat@gmail.com>
2025-08-21 18:30:02 +0330

    load custom cursor

--- Scripts/Root.cs
+++ Scripts/Root.cs
@@ -2,9 +2,12 @@ using Godot;
 
 public partial class Root : Node2D
 {
+    private readonly Resource mouse_cursor = ResourceLoader.Load("./Assets/Cursor/Swords_1/Iicon_32_01.png");
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Input.SetCustomMouseCursor(mouse_cursor);
         GD.Print("Hello from C#");
     }
 
